363d3af10566d98c1a524f2865b74ccca0f20695
tiantsing <tiantsing@126.com>
2019-11-05 22:15:02 +0800

    tiantsing
    2019

--- simple/src/main/java/tk/mybatis/simple/mapper/UserMapper.java
+++ simple/src/main/java/tk/mybatis/simple/mapper/UserMapper.java
@@ -105,4 +105,99 @@ public interface UserMapper {
 	* 2019年10月14日     tianq           v1.0.0               修改原因
 	 */
 	int insert(SysUser sysUser);
+	/**
+	 * 
+	* @Function: UserMapper.java
+	* @Description: 使用JDBC的方式获得主键自增的值
+	*
+	* @param:描述1描述
+	* @return：返回结果描述
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: tianq
+	* @date: 2019年10月16日 下午11:05:50 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2019年10月16日     tianq           v1.0.0               修改原因
+	 */
+	int insert2(SysUser sysUser);
+	/**
+	 * 
+	* @Function: UserMapper.java
+	* @Description: 使用selectKey返回主键的值
+	*
+	* @param:描述1描述
+	* @return：返回结果描述
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: tianq
+	* @date: 2019年10月16日 下午11:14:58 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2019年10月16日     tianq           v1.0.0               修改原因
+	 */
+	int insert3(SysUser sysUser);
+	/**
+	 * 
+	* @Function: UserMapper.java
+	* @Description: 根据主键更新
+	*
+	* @param:描述1描述
+	* @return：返回结果描述
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: tianq
+	* @date: 2019年10月21日 下午10:10:42 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2019年10月21日     tianq           v1.0.0               修改原因
+	 */
+	int updateById(SysUser sysUser);
+	/**
+	 * 
+	* @Function: UserMapper.java
+	* @Description: 根据主键删除
+	*
+	* @param:描述1描述
+	* @return：返回结果描述
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: tianq
+	* @date: 2019年10月22日 下午11:07:25 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2019年10月22日     tianq           v1.0.0               修改原因
+	 */
+	int deleteById(Long id);
+	/**
+	 * 
+	* @Function: UserMapper.java
+	* @Description: 动态条件查询用户信息
+	*
+	* @param:描述1描述
+	* @return：返回结果描述
+	* @throws：异常描述
+	*
+	* @version: v1.0.0
+	* @author: tianq
+	* @date: 2019年10月28日 下午10:43:39 
+	*
+	* Modification History:
+	* Date         Author          Version            Description
+	*---------------------------------------------------------*
+	* 2019年10月28日     tianq           v1.0.0               修改原因
+	 */
+	List<SysUser> selectByUser(SysUser sysUser);
 }
--- simple/src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
+++ simple/src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -8,6 +8,7 @@
  */
 package tk.mybatis.simple.mapper;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -73,6 +74,123 @@ public class UserMapperTest extends BaseMapperTest{
 		    Assert.assertNotNull(roleList);
 		    Assert.assertTrue(roleList.size()>0);
 		}finally {
+			sqlSession.close();
+		}
+	}
+	@Test
+	public void testInsert() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			SysUser user = new SysUser();
+			user.setUserName("test1");
+			user.setUserPassword("123456");
+			user.setUserEmail("test@mybatis.com");
+			user.setUserInfo("test info");
+			user.setHeadImg(new byte[] {1,2,3});
+			user.setCreateTime(new Date());
+			int result = userMapper.insert(user);
+			Assert.assertEquals(1,result);
+			Assert.assertNull(user.getId());
+		}finally {
+			sqlSession.rollback();
+			sqlSession.close();
+		}
+	}
+	@Test
+	public void testInsert2() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			SysUser user = new SysUser();
+			user.setUserName("test1");
+			user.setUserPassword("123456");
+			user.setUserEmail("test@mybatis.com");
+			user.setUserInfo("test info");
+			user.setHeadImg(new byte[] {1,2,3});
+			user.setCreateTime(new Date());
+			int result = userMapper.insert2(user);
+			Assert.assertEquals(1,result);
+			Assert.assertNotNull(user.getId());
+		}finally {
+			sqlSession.rollback();
+			sqlSession.close();
+		}
+	}
+	@Test
+	public void testInsert3() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			SysUser user = new SysUser();
+			user.setUserName("test1");
+			user.setUserPassword("123456");
+			user.setUserEmail("test@mybatis.com");
+			user.setUserInfo("test info");
+			user.setHeadImg(new byte[] {1,2,3});
+			user.setCreateTime(new Date());
+			int result = userMapper.insert3(user);
+			Assert.assertEquals(1,result);
+			Assert.assertNotNull(user.getId());
+		}finally {
+			sqlSession.commit();
+			sqlSession.close();
+		}
+	}
+	@Test
+	public void testUpdateById() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			SysUser user = userMapper.selectById(1L);
+			Assert.assertEquals("admin", user.getUserName());
+			user.setUserName("admin_test");
+			user.setUserEmail("test@mybatis.tk");
+			int result = userMapper.updateById(user);
+			Assert.assertEquals(1,result);
+			user = userMapper.selectById(1L);
+			Assert.assertEquals("admin_test", user.getUserName());
+		}finally {
+			sqlSession.rollback();
+			sqlSession.close();
+		}
+	}
+	@Test
+	public void testDeleteById() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			SysUser user1 = userMapper.selectById(1L);
+			Assert.assertNotNull(user1);
+			Assert.assertEquals(1, userMapper.deleteById(1L));
+			Assert.assertNull(userMapper.selectById(1L));
+		}finally {
+			sqlSession.rollback();
+			sqlSession.close();
+		}
+	}
+	@Test
+	public void testSelectByUser() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			//只查询用户名
+			SysUser query = new SysUser();
+			query.setUserName("ad");
+			List<SysUser> userList = userMapper.selectByUser(query);
+			Assert.assertTrue(userList.size()>0);
+			//只查询用户邮箱时
+			query = new SysUser();
+			query.setUserEmail("test@mybatis.tk");
+			userList = userMapper.selectByUser(query);
+			Assert.assertTrue(userList.size()>0);
+			//当同事查询用户名和邮箱时
+			query = new SysUser();
+			query.setUserName("ad");
+			query.setUserEmail("test@mybatis.tk");
+			userList = userMapper.selectByUser(query);
+			Assert.assertTrue(userList.size() == 0);
+ 		}finally {
 			sqlSession.close();
 		}
 	}
